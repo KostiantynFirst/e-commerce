9ef9cc49adc832bfa55e93c85029c63712ef7bcd
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-12-11 23:06:57 +0200

    Update NavItems.tsx

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import NavItem from "./NavItem";
 
 const NavItems = () => {
@@ -9,6 +9,8 @@ const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null)
    
     const isAnyOpen = activeIndex !== null
+    
+    const navRef = useRef<HTMLDivElement | null>(null)
 
     return (
         <div className='flex gap-4 h-full'>
