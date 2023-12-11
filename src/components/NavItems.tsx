12fb969c19f49e5d8723ecdee1c1da18c1cec13b
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-12-11 23:11:29 +0200

    Update NavItems.tsx

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -3,6 +3,7 @@
 import { PRODUCT_CATEGORIES } from "@/config";
 import { useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
 
@@ -12,8 +13,10 @@ const NavItems = () => {
     
     const navRef = useRef<HTMLDivElement | null>(null)
 
+    useOnClickOutside(navRef, () => setActiveIndex(null))
+
     return (
-        <div className='flex gap-4 h-full'>
+        <div className='flex gap-4 h-full' ref={navRef}>
             {PRODUCT_CATEGORIES.map((category, i) => {
 
 
