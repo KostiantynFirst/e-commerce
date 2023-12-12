3a4d8a7509643cabab35fcf4a268da61dd0d10b2
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-12-12 23:35:02 +0200

    Update Navbar & create Cart element

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,8 @@ import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
 
+import Cart from "./Cart";
+
 
 const Navbar = () => {
 
@@ -74,6 +76,8 @@ const Navbar = () => {
                             </div>
                             )}
 
+                            <div className="ml-4 flow-root lg:ml-6"></div>
+                                <Cart />   
                             </div>
                         </div>
 
