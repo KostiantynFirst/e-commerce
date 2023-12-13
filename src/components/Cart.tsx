680804d7bc7b94e8c9318f2185915a3729d9db45
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-12-13 22:49:27 +0200

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -108,44 +108,7 @@ const Cart = () => {
           </SheetTrigger>
         </div>
          )}
-
-              
-
-              
-       {/*    </div>
-          </>
-        ) : (
-          <div className='flex h-full flex-col items-center justify-center space-y-1'>
-            <div
-              aria-hidden='true'
-              className='relative mb-4 h-60 w-60 text-muted-foreground'>
-              <Image
-                src='/hippo-empty-cart.png'
-                fill
-                alt='empty shopping cart'
-              />
-            </div>
-            <div className='text-xl font-semibold'>
-              Your cart is empty
-            </div>
-            <SheetTrigger asChild>
-              <Link
-                href='/products'
-                className={buttonVariants({
-                  variant: 'link',
-                  size: 'sm',
-                  className:
-                    'text-sm text-muted-foreground',
-                })}>
-                Add items to your cart to checkout
-              </Link>
-            </SheetTrigger>
-          </div>
-        )} */}
-
-
-
-        </SheetContent>
+         </SheetContent>
     </Sheet>
 }
 
