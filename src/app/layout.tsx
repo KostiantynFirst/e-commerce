4bec2768920261f4ae6cf8cd144cce5c243b5ba0
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-12-18 02:50:46 +0200

    add trpc stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
 import Navbar from '@/components/Navbar'
+import Providers from '@/components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,8 +21,12 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body className={cn('relative h-full font-sans antialiased', inter.className)}>
         <main className='relative flex flex-col min-h-screen'>
+          <Providers>
           <Navbar />
-          <div className='flex-grow flex-1'>{children}</div>
+          <div className='flex-grow flex-1'>
+            {children}
+          </div>
+          </Providers>
         </main>
       </body>
     </html>
