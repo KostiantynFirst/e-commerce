ad482d1d6d7a0479f3ba2576407090b38d937476
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2024-01-07 22:56:21 +0200

    add verify-email components

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { AuthCredentialsValidator } from '../lib/validators/account-credentials-
 import { publicProcedure, router } from './trpc'
 import { getPayloadClient } from '../get-payload'
 import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
 
 export const authRouter = router({
   createPayloadUser: publicProcedure
@@ -35,23 +36,23 @@ export const authRouter = router({
       return { success: true, sentToEmail: email }
     }),
 
-//   verifyEmail: publicProcedure
-//     .input(z.object({ token: z.string() }))
-//     .query(async ({ input }) => {
-//       const { token } = input
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input
 
-//       const payload = await getPayloadClient()
+      const payload = await getPayloadClient()
 
-//       const isVerified = await payload.verifyEmail({
-//         collection: 'users',
-//         token,
-//       })
+      const isVerified = await payload.verifyEmail({
+        collection: 'users',
+        token,
+      })
 
-//       if (!isVerified)
-//         throw new TRPCError({ code: 'UNAUTHORIZED' })
+      if (!isVerified)
+        throw new TRPCError({ code: 'UNAUTHORIZED' })
 
-//       return { success: true }
-//     }),
+      return { success: true }
+    }),
 
 //   signIn: publicProcedure
 //     .input(AuthCredentialsValidator)
