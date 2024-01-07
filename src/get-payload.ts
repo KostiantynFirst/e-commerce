9af81db63707c1795a10905c75a4f415d556c6b9
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2024-01-07 19:53:02 +0200

    Update get-payload.ts

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -3,20 +3,21 @@ import path from "path"
 import type { InitOptions } from 'payload/config'
 import payload from "payload"
 import { Payload } from "payload"
+import nodemailer from "nodemailer"
 
 dotenv.config({
     path: path.resolve(__dirname, '../.env'),
   })  
   
-//   const transporter = nodemailer.createTransport({
-//     host: 'smtp.resend.com',
-//     secure: true,
-//     port: 465,
-//     auth: {
-//       user: 'resend',
-//       pass: process.env.RESEND_API_KEY,
-//     },
-//   })
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.resend.com',
+    secure: true,
+    port: 465,
+    auth: {
+      user: 'resend',
+      pass: process.env.RESEND_API_KEY,
+    },
+  })
   
   let cached = (global as any).payload
   
@@ -44,11 +45,11 @@ dotenv.config({
   
     if (!cached.promise) {
       cached.promise = payload.init({
-        // email: {
-        //   transport: transporter,
-        //   fromAddress: 'hello@joshtriedcoding.com',
-        //   fromName: 'DigitalHippo',
-        // },
+        email: {
+          transport: transporter,
+          fromAddress: 'onboarding@resend.com',
+          fromName: 'DigitalCommerce',
+        },
         secret: process.env.PAYLOAD_SECRET,
         local: initOptions?.express ? false : true,
         ...(initOptions || {}),
