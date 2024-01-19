8309b1c2c134ccb6bff85e057cf58fb3e3b044a2
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2024-01-19 14:01:06 +0200

    changes for sign-up and layout

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -11,23 +11,55 @@ import { cn } from "@/lib/utils"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator"
 import { trpc } from "@/trpc/client"
+import { toast } from "sonner"
+import { ZodError } from "zod"
+import { useRouter } from "next/navigation"
 
 const Page = () => {
-
-    const { register, handleSubmit, formState: {errors} } = useForm<TAuthCredentialsValidator>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({
     resolver: zodResolver(AuthCredentialsValidator),
   })
 
-  // const { data } = trpc.anyApiRoute.useQuery()
-  // console.log(data)
+  const router = useRouter()
 
-  const {mutate, isLoading} = trpc.auth.createPayloadUser.useMutation({
+  const { mutate, isLoading } =
+    trpc.auth.createPayloadUser.useMutation({
+      onError: (err) => {
+        if (err.data?.code === 'CONFLICT') {
+          toast.error(
+            'This email is already in use. Sign in instead?'
+          )
 
-  })
+          return
+        }
 
+        if (err instanceof ZodError) {
+          toast.error(err.issues[0].message)
 
-  const onSubmit = ({email, password}: TAuthCredentialsValidator) => {
-    mutate({email, password})
+          return
+        }
+
+        toast.error(
+          'Something went wrong. Please try again.'
+        )
+      },
+      onSuccess: ({ sentToEmail }) => {
+        toast.success(
+          `Verification email sent to ${sentToEmail}.`
+        )
+        router.push('/verify-email?to=' + sentToEmail)
+      },
+    })
+
+  const onSubmit = ({
+    email,
+    password,
+  }: TAuthCredentialsValidator) => {
+    mutate({ email, password })
   }
 
     return (
@@ -63,6 +95,12 @@ const Page = () => {
                         })}
                         placeholder='you@example.com'
                       />
+                      {errors?.email && (
+                        <p className="text-sm text-red-500">
+                          {errors.email.message}
+                        </p>
+                      )}
+
                     </div>
                     <div className='grid gap-1 py-2'>
                       <Label htmlFor='password'>Password</Label>
@@ -74,6 +112,12 @@ const Page = () => {
                         })}
                         placeholder='Password'
                       />
+                        {errors?.password && (
+                        <p className="text-sm text-red-500">
+                          {errors.password.message}
+                        </p>
+                      )}
+                      
                     </div>
 
                     <Button>Sign up</Button>
